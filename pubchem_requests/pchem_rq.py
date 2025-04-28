7ebe5b375bf8736048b36e76a3688f421da54158
BPinsard-epfl <bastien.pinsard@epfl.ch>
2025-04-29 00:56:48 +0200

    "removed" the getPkaFromSmiles function, made a new, better function

--- pubchem_requests/pchem_rq.py
+++ pubchem_requests/pchem_rq.py
@@ -2,15 +2,12 @@ import requests
 import json
 import regex as re
 
-# r = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/165/JSON/")
-# print(r.text)
-
-def getPugRestJSONfromSmiles(smiles):
+""" def getPugRestJsonFromSmiles(smiles):
     url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/" + smiles + "/JSON"
     req = requests.get(url)
-    return req.text
+    return req.text """
 
-def getPkaFromSmiles(smiles):
+""" def getPkaFromSmiles(smiles):
     url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/" + smiles + "/cids/TXT"
     cid = int(requests.get(url).text)
     url1 = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/" + str(cid) + "/JSON/?heading=Dissociation+Constants"
@@ -20,7 +17,30 @@ def getPkaFromSmiles(smiles):
         pka = x['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][0]['Value']['StringWithMarkup'][0]['String']
         return float(re.search(r'\d+\.\d+', pka).group())
     except:
-        return "No pKa was found"
+        return "No pKa was found" """
+    
+def getMoleculeInfoFromSmiles(smiles):
+    req = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/" + smiles + "/JSON")
+    try:
+        mol = json.loads(req.text)['PC_Compounds'][0]
+    except:
+        mol = json.loads(req.text)['Fault']
+        raise Exception(mol['Code'] + "\n" + mol['Message'])
+    props = mol['props']
+    req = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/" + str(mol['id']['id']['cid']) + "/JSON/?heading=Dissociation+Constants")
+    try:
+        x = json.loads(req.text)['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][0]['Value']['StringWithMarkup'][0]['String']
+        pka = float(re.search(r'\d+\.\d+', x).group())
+    except:
+        pka = None
+    molProperties = {
+        "name": props[6]['value']['sval'], 
+        "cid": int(mol['id']['id']['cid']), 
+        "smiles": props[20]['value']['sval'], 
+        "logP": float(props[14]['value']['fval']), 
+        "pKa": pka}
+    return molProperties
     
 
-print(getPkaFromSmiles("CCO"))
+print(getMoleculeInfoFromSmiles("O=C(O)c1c(C(O)=O)cccc1"))
+print(getMoleculeInfoFromSmiles("CCO"))
