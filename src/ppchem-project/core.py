67874fa5aa52830bb85cb392a43a988c0ff112b9
Raphtsr <raphael.tisseyre@epfl.ch>
2025-05-06 11:39:08 +0200

    Add findLogPGaps function and documentations

--- src/ppchem-project/core.py
+++ src/ppchem-project/core.py
@@ -14,7 +14,7 @@ data = pd.read_csv(datapath)
 def SmilesDataFrame(file : str, sep_ : str = ";", skiprows_ : int | Sequence[int] | bool = False, skipblanklines_ : bool = True) -> pd.DataFrame:
     """
     This function will create the dataframe thanks to the file given.
-    It will be then used in further functions.    
+    It will be then used in further functions.
     """
     
     p = Path(".")
@@ -36,6 +36,18 @@ def SmilesDataFrame(file : str, sep_ : str = ";", skiprows_ : int | Sequence[int
 
 def givesDataFrame(file : str, sep_ : str = ";", skiprows_ : int | Sequence[int] | bool = False, skipblanklines_ : bool = True) -> pd.DataFrame:
     """
+    Take a list of smile from a file and return a dataframe 
+    with proprieties of each molecule according to PubChem.
+
+    The file should a CSV table with one column of smiles.
+
+    The Given data frames have for entries :
+    name, canonical smile, CAS, formula, mol weight,
+    pKa, logP, formal charge and sterimol.
+
+    When no pKa is found from pubchem it is predicted with 
+    PypKa package.
+    Sterimol is calculated thanks to wSterimol package.
     """
 
     df_smiles = df_smiles(file, sep_, skiprows_, skipblanklines_)
@@ -61,7 +73,7 @@ def givesDataFrame(file : str, sep_ : str = ";", skiprows_ : int | Sequence[int]
 def findpKaGaps(df : pd.DataFrame, nb : int = 1) -> dict[float, tuple[int, int]]:
     """
     A function used to find the biggest pka gaps of a dataframe. 
-    It will give by defalt the biggest gap, but one can precise how many he wants.
+    It will give by default the biggest gap, but one can precise how many he wants.
     """
 
     sorted_df = df.sort_values(by='pKa')
@@ -79,7 +91,29 @@ def findpKaGaps(df : pd.DataFrame, nb : int = 1) -> dict[float, tuple[int, int]]
     
     return maxs_dic
 
+
+def findLogPGaps(df : pd.DataFrame, nb : int = 1) -> dict[float, tuple[int, int]]:
+    """
+    A function used to find the biggest logP gaps of a dataframe. 
+    It will give by default the biggest gap, but one can precise how many he wants.
+    """
+
+    sorted_df = df.sort_values(by='logP')
+    maxs : list[tuple[float, tuple[int, int]]] = [(0,(0,0)) for x in range(nb)]
+
+    for i in range(len(sorted_df["logP"])-1):
+        for j in range(len(maxs)):
+            if maxs[j] < (sorted_df["logP"].iloc(i) - sorted_df["logP"].iloc(i+1)):
+                maxs[j] = (sorted_df["logP"].iloc(i) - sorted_df["logP"].iloc(i+1), (i, i+1))
+                break
     
+    maxs_dic : dict[float, tuple[int, int]]
+    for i in range(len(maxs)):
+        maxs_dic[maxs[i][0]] = maxs[i][1]
+    
+    return maxs_dic
+ 
+
 def findAcidOrBase(pka : float = m.inf, smiles : str = "", logP : float = m.inf, nu : str = "") -> pd.DataFrame:
     """
     Gives a specific acid based on Smiles, pka, Nucleophilicity, ...
