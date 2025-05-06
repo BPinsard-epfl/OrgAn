f49b5c3dbe78d67b709eb6b0fe4bc5f3d5a88eb6
BPinsard-epfl <bastien.pinsard@epfl.ch>
2025-05-06 23:48:56 +0200

    Change: updated the getMoleculeInfoFromSmiles function

--- pubchem_requests/pchem_rq.py
+++ pubchem_requests/pchem_rq.py
@@ -20,25 +20,54 @@ import regex as re
         return "No pKa was found" """
     
 def getMoleculeInfoFromSmiles(smiles):
+    # This function takes a SMILES string as an input, and returns a Python Dictionary as an output, containing the properties shown in molProperties
     req = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/" + smiles + "/JSON")
     try:
         mol = json.loads(req.text)['PC_Compounds'][0]
     except:
         mol = json.loads(req.text)['Fault']
         raise Exception(mol['Code'] + "\n" + mol['Message'])
+    molProperties = {
+        "name": None, 
+        "cid": mol['id']['id']['cid'], 
+        "CASno": None,
+        "smiles": None, 
+        "molWeight": None,
+        "molFormula": None,
+        "logP": None, 
+        "pKa": None,
+        "charge": mol["charge"],
+        "sterimol": None}
     props = mol['props']
+    i = 0
+    while True: # This might be a bit of a barbaric approach but hey, if it works, it works. Basically I run the loop until the index goes out of range, which will raise an exception, indicating we've reached the end of the list.
+        try:
+            currentProperty = props[i]["urn"]
+            val = props[i]["value"]
+        except:
+            break
+        i += 1
+        try:
+            if currentProperty["label"] not in ["IUPAC Name", "Log P", "Molecular Weight", "Molecular Formula", "SMILES"]: 
+                continue
+            elif currentProperty["label"] == "Molecular Formula":
+                molProperties["molFormula"] = val["sval"]
+            elif currentProperty["label"] == "Molecular Weight":
+                molProperties["molWeight"] = float(val["sval"])
+            elif currentProperty["label"] == "Log P":
+                molProperties["logP"] = val["fval"]
+            elif currentProperty["name"] == "Preferred":
+                molProperties["name"] = val["sval"]
+            elif currentProperty["name"] == "Canonical":
+                molProperties["smiles"] = val["sval"]
+        except:
+            continue
     req = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/" + str(mol['id']['id']['cid']) + "/JSON/?heading=Dissociation+Constants")
     try:
         x = json.loads(req.text)['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][0]['Value']['StringWithMarkup'][0]['String']
-        pka = float(re.search(r'\d+\.\d+', x).group())
+        molProperties["pKa"] = float(re.search(r'\d+\.\d+', x).group())
     except:
-        pka = None
-    molProperties = {
-        "name": props[6]['value']['sval'], 
-        "cid": int(mol['id']['id']['cid']), 
-        "smiles": props[20]['value']['sval'], 
-        "logP": float(props[14]['value']['fval']), 
-        "pKa": pka}
+        molProperties["pKa"] = None
     return molProperties
     
 
