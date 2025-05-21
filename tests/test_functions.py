033bfb0d73facc15948ae9d94ca569c92baf602d
Raphtsr <raphael.tisseyre@epfl.ch>
2025-05-21 12:46:29 +0200

    update __init__

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,4 +4,4 @@ from OrgAn.functions import gives_data_frame
 def test_gives_dataframe():
     assert gives_data_frame("test_smiles.csv") == pd.read_csv("test_data.csv"), "givesDataFrame do not give the correct answer"
 
-test_gives_dataframe
+test_gives_dataframe()
